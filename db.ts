7b2233b0d4ee92e0f5a76b2cdf107f827d609b1f
KilianBrd <kilianberaud3@gmail.com>
2023-09-26 11:49:15 +0200

    s

--- db.ts
+++ db.ts
@@ -3,14 +3,15 @@ import mongoose from 'mongoose';
 
 // Remplacez les valeurs entre < > par vos informations
 const connectionString =
-  'mongodb+srv://<username>:<password>@cluster0.o7wjc5i.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://kilianbrd:coucou@dbclusterkilian.ynsofdk.mongodb.net/?retryWrites=true&w=majority';
 
-export const connectToDatabase = async (): Promise<void> => {
-  try {
-    await mongoose.connect(connectionString);
-    console.log('Connecté à MongoDB Atlas');
-  } catch (error) {
-    console.error('Erreur de connexion à MongoDB :', error);
-    process.exit(1); // Quitte l'application en cas d'erreur de connexion
-  }
-};
+  export const connectToDatabase = async (): Promise<void> => {
+    try {
+      await mongoose.connect(connectionString);
+      console.log('Connecté à MongoDB Atlas');
+    } catch (error) {
+      console.error('Erreur de connexion à MongoDB :', error);
+      process.exit(1); // Quitte l'application en cas d'erreur de connexion
+    }
+  };
+  
